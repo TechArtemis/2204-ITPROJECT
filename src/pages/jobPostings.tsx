386fb3006ece7ee7c7ea7989e7f1175ca084fb62
LorenzCastillo <lorenz5524@hotmail.com>
2023-01-26 23:58:18 -0800

    Fully implemented Tailwind (forgot to make the config file for it with npx) and currently working on the jobPostings Page

---
+++ src/pages/jobPostings.tsx
@@ -0,0 +1,16 @@
+// Third-party Import
+import Image from "next/image";
+// Local imports
+import styles from "@/styles/Header.module.sass";
+import vccLogo from "@/../public/images/vccLogo.png";
+import avatar from "@/../public/images/avatar.png";
+import Header from "@/components/jobHeader";
+
+
+export default function Home() {
+    return (
+        <>
+            <Header/>
+        </>
+    );
+};
