f42120dab332de4dd2ed348e8e49640a5527552c
arejandro <a.jmartin0229@gmail.com>
2023-02-07 02:49:34 -0800

    regex and createStudent fix

--- src/shared/regex.ts
+++ src/shared/regex.ts
@@ -2,7 +2,7 @@
 /**
  * email pattern
  */
-export const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&’+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)$/;
+export const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&’+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)+(?:.[a-zA-Z0-9-]+)$/;
 
 /**
   * phone number pattern
