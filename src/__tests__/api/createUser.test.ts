9687057e796c16a149606268608d346ae5d91231
emilsangil <sangilemiljan@gmail.com>
2023-03-16 21:07:44 -0700

    Fixed the test

--- src/__tests__/api/createUser.test.ts
+++ src/__tests__/api/createUser.test.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest } from "next";
 import handler from "@/pages/api/user/create";
 import { getUser, deleteUser } from "@/backend/actions/user";
-import { User } from "@/interface/User";
+import { Model as userModel } from "@/backend/database/ODM/User";
 
 describe("createUser API", () => {
 
@@ -29,15 +29,6 @@ describe("createUser API", () => {
         });
     });
 
-    it("Should return code 400 Alumni already exists", async () => {
-        const email = "000451777@student.vcc.ca";
-        const res = getUser(email);
-
-        expect((await res).code).toBe(400);
-        expect((await res).message).toBe("User not registered");
-
-    });
-
     it("Should return 200 if all fields are filled and passes the requirements", async () => {
         const req = {
             method: "POST",
@@ -61,5 +52,30 @@ describe("createUser API", () => {
         expect(res.json).toHaveBeenCalledWith({
             message: "User created",
         });
+    });
+
+    it("Should return code 400 if user already exists", async () => {
+        const req = {
+            method: "POST",
+            body: {
+                user: {
+                    name: "Test John",
+                    email: "000451777@student.vcc.ca",
+                    password: "Test12345!",
+                },
+            },
+        } as NextApiRequest;
+
+        const res: any = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn(),
+        };
+
+        await handler(req, res);
+        expect(res.status).toHaveBeenCalledWith(400);
+        expect(res.json).toHaveBeenCalledWith({
+            message: "Alumni already exists",
+        });
+
     });
 });
