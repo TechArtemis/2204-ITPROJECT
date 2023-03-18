3b0058037104215bddff20fdd390a13de180d04e
arejandro <jandrawmartin@gmail.com>
2023-03-18 11:41:03 -0700

    createAdmin test

--- src/__tests__/api/createAdmin.test.ts
+++ src/__tests__/api/createAdmin.test.ts
@@ -1,8 +1,25 @@
 import handler from "@/pages/api/admin";
 import { NextApiRequest } from "next";
+import { Model as adminModel } from "@/backend/database/ODM/Admin";
+import Database from "@/backend/database";
+import { Admin } from "@/interface/Admin";
+import { createAdmin } from "@/backend/actions/admin";
 
-describe("createAdmin API", () => {
+describe("createUser API", () => {
 
+    // beforeAll(() => {
+    //     const admin: Admin = {
+    //         email: "test",
+    //         password: "pass.123",
+
+    //     };
+    //     createAdmin(admin);
+    // });
+
+    afterAll( async() => {
+        await adminModel.findOneAndDelete({ email: "test@mail.co" });
+        Database.disconnect();
+    });
     it("Should return code 400 if email is missing or invalid", async () => {
         const req = {
             method: "POST",
@@ -49,6 +66,7 @@ describe("createAdmin API", () => {
             message: "Successful",
         });
     });
+
 
     it("Should return code 400 if Admin already exists", async () => {
         const req = {
--- src/interface/Admin.ts
+++ src/interface/Admin.ts
@@ -11,5 +11,5 @@ export interface Admin {
     _id?: Types.ObjectId,
     email: string,
     password: string,
-    name: "Admin"
+    name?: "Admin"
 }
