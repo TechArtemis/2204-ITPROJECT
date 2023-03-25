9f09d94adf99a7380a11ad3e1a90be45fb2bfb8e
arejandro <jandrawmartin@gmail.com>
2023-03-24 18:00:58 -0700

    push

--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -35,7 +35,7 @@ interface Props {
     job: string,
     type: string,
     liked: boolean,
-	tags: string,
+	tags: string[],
     children?: ReactNode;
     className?: string;
     extraFunction?: (jobID: string) => void;
@@ -79,11 +79,18 @@ export default function Card(props: Props) {
 			</div>
 
 			<div className={styles.jobInfo} onClick={() => handleClick()}>
-				<h1>{props.job}</h1>
-				<h2>{props.type}</h2>
-				<div className={styles.tags}>
-					<p>{props.tags}</p>
-				</div>
+				<>
+					<h1>{props.job}</h1>
+					<h2>{props.type}</h2>
+					{ props.tags.map((item, i) => {
+						<div className={styles.tags} key={i}>
+							<p>{item}</p>
+						</div>;
+					})
+					}
+				</>
+
+
 			</div>
 			<div>
 				{!liked ?
