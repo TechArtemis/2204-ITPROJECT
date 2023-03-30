aea635487db0f248699e1f5fd1e93be1bb502c2f
emilsangil <sangilemiljan@gmail.com>
2023-03-30 01:47:57 -0700

    Implements Project Schema and functions
    Co-authored-by: Alejandro Martin <jandrawmartin@gmail.com>

--- src/backend/actions/cloudinary/index.ts
+++ src/backend/actions/cloudinary/index.ts
@@ -3,27 +3,27 @@ import cloudinary from "@/shared/cloudinary";
 const { uploader } = cloudinary;
 
 export async function uploadFile(path: string) {
-    try {
-        const response = await uploader.upload(
-            path,
-            {
-                // eslint-disable-next-line camelcase
-                unique_filename: true
-            }
-        );
-        if (!response) {
-            throw {
-                code: 500,
-                message: `failed to upload file with path: ${path}`
-            };
-        }
+	try {
+		const response = await uploader.upload(
+			path,
+			{
+				// eslint-disable-next-line camelcase
+				unique_filename: true
+			}
+		);
+		if (!response) {
+			throw {
+				code: 500,
+				message: `failed to upload file with path: ${path}`
+			};
+		}
 
-        return response;
+		return response;
 
-    } catch (error: any) {
-        throw{
-            error
-        };
+	} catch (error: any) {
+		throw{
+			error
+		};
 
-    }
+	}
 }
